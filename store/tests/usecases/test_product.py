4c94bbdf90fbae91663b432f6d1bd18c86c5f861
Carlos Chiarelli <ca.chiarelli.97@gmail.com>
2024-07-08 08:47:09 -0300

    feat: add store project

---
+++ store/tests/usecases/test_product.py
@@ -0,0 +1,16 @@
+from typing import List
+from uuid import UUID
+
+import pytest
+
+# from store.core.exceptions import NotFoundException
+# from store.schemas.product import ProductOut
+# from store.schemas.product import ProductOut, ProductUpdateOut
+from store.usecases.product import product_usecase
+
+
+async def test_usecases_create_should_return_success(product_in):
+    result = await product_usecase.create(body=product_in)
+
+    # assert isinstance(result, ProductOut)
+    assert result is None
